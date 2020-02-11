6ff9bfcd150e61c2d33d34a1e6f69b008dbd02c8
Damian Nolan <damiannolan@gmail.com>
2020-02-11 21:51:28 +0000

    Added routes boilerpalte

---
+++ auth/routes.go
@@ -0,0 +1,31 @@
+package auth
+
+import "net/http"
+
+func (mx *Mux) authorize(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func (mx *Mux) callback(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func (mx *Mux) expired(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func (mx *Mux) health(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func (mx *Mux) login(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func (mx *Mux) logout(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func (mx *Mux) token(w http.ResponseWriter, req *http.Request) {
+
+}
