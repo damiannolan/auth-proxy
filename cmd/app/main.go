011821dafa672b2e91b990f27350dd46d2f3f978
Damian Nolan <damiannolan@gmail.com>
2020-02-11 23:07:39 +0000

    Serve authMux in cmd/app entrypoint

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,21 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/damiannolan/auth-proxy/auth"
 )
 
 func main() {
-	fmt.Println("hello world...")
-
-	http.HandleFunc("/oauth/authorize", nil)
-	http.HandleFunc("/oauth/callback", nil)
-	http.HandleFunc("/oauth/expired", nil)
-	http.HandleFunc("/oauth/health", nil)
-	http.HandleFunc("/oauth/login", nil)
-	http.HandleFunc("/oauth/logout", nil)
-	http.HandleFunc("/oauth/authorize", nil)
-	http.HandleFunc("/oauth/token", nil)
-
-	http.ListenAndServe(":8080", nil)
+	authMux := auth.NewMux("/oauth")
+	http.ListenAndServe(":8080", authMux.Handler())
 }
