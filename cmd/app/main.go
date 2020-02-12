d3b01e0fbcb2594915609c557da41d8484c64d39
Damian Nolan <damiannolan@gmail.com>
2020-02-12 23:08:34 +0000

    Clean up comments

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -9,15 +9,16 @@ import (
 )
 
 func main() {
-	viper.SetConfigName("application") // name of config file (without extension)
-	viper.SetConfigType("yaml")        // REQUIRED if the config file does not have the extension in the name
-	viper.AddConfigPath("config")      // path to look for the config file in
-	viper.AddConfigPath(".")           // optionally look for config in the working directory
+	viper.SetConfigName("application")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("config")
+	viper.AddConfigPath(".")
 	if err := viper.ReadInConfig(); err != nil {
 		panic(err)
 	}
 
 	log.WithFields(log.Fields{"port": viper.GetString("proxy.port"), "server": viper.GetString("proxy.host")}).Info("starting application server")
+
 	authMux := auth.NewMux("/oauth")
 	http.ListenAndServe(":"+viper.GetString("proxy.port"), authMux.Handler())
 }
