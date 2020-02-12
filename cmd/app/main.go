39d891d56a3fb83adcab83956d068480d0a58eba
Damian Nolan <damiannolan@gmail.com>
2020-02-12 23:07:50 +0000

    Read in config using viper

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,9 +4,20 @@ import (
 	"net/http"
 
 	"github.com/damiannolan/auth-proxy/auth"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	viper.SetConfigName("application") // name of config file (without extension)
+	viper.SetConfigType("yaml")        // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("config")      // path to look for the config file in
+	viper.AddConfigPath(".")           // optionally look for config in the working directory
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+
+	log.WithFields(log.Fields{"port": viper.GetString("proxy.port"), "server": viper.GetString("proxy.host")}).Info("starting application server")
 	authMux := auth.NewMux("/oauth")
-	http.ListenAndServe(":8080", authMux.Handler())
+	http.ListenAndServe(":"+viper.GetString("proxy.port"), authMux.Handler())
 }
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/coreos/go-oidc v2.2.1+incompatible
 	github.com/go-chi/chi v4.0.3+incompatible
 	github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
-	github.com/sirupsen/logrus v1.2.0
+	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/viper v1.6.2
 	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
 	gopkg.in/square/go-jose.v2 v2.4.1 // indirect
