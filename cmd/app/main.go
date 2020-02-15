4d9850cb54b7c6f8268e9f8548a52700fe028973
Damian Nolan <damiannolan@gmail.com>
2020-02-15 08:41:58 +0000

    Added boilerplate code for changing log level at runtime. TODO: cleanup to own func

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/damiannolan/auth-proxy/auth"
+	"github.com/fsnotify/fsnotify"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
+
+func init() {
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp:   true,
+		TimestampFormat: time.RFC3339Nano,
+	})
+	log.SetLevel(log.DebugLevel)
+	log.SetOutput(os.Stdout)
+}
 
 func main() {
 	viper.SetConfigName("application")
@@ -16,6 +28,21 @@ func main() {
 	if err := viper.ReadInConfig(); err != nil {
 		panic(err)
 	}
+
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		log.WithFields(log.Fields{"file": e.Name, "op": e.Op.String()}).Debug("Config file changed")
+
+		lvl, err := log.ParseLevel(viper.GetString("logger.level"))
+		if err != nil {
+			log.WithError(err).WithField("level", log.DebugLevel).Info("setting default logger level")
+			log.SetLevel(log.DebugLevel)
+			return
+		}
+
+		log.WithField("level", viper.GetString("logger.level")).Info("setting logger level")
+		log.SetLevel(lvl)
+	})
 
 	log.WithFields(log.Fields{"port": viper.GetString("services.auth-proxy.port"), "server": viper.GetString("services.auth-proxy.host")}).Info("starting application server")
 
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/coreos/go-oidc v2.2.1+incompatible
+	github.com/fsnotify/fsnotify v1.4.7
 	github.com/go-chi/chi v4.0.3+incompatible
 	github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
 	github.com/sirupsen/logrus v1.4.2
