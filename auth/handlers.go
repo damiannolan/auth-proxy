ca8bd5036c526d1a1e597a35467bc5dde8e4d83e
Damian Nolan <damiannolan@gmail.com>
2020-02-16 00:02:36 +0000

    Updated authorize and callback handlers to acquire access token

--- auth/handlers.go
+++ auth/handlers.go
@@ -1,54 +1,78 @@
 package auth
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
 
+	"github.com/damiannolan/auth-proxy/openid"
 	"github.com/damiannolan/auth-proxy/realm"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
 func (mx *Mux) authorize(w http.ResponseWriter, req *http.Request) {
 	realmID, ok := realm.FromContext(req.Context())
 	if !ok {
-		// redirect
+		redirectURL := "getRedirectURL()"
+		http.Redirect(w, req, "", http.StatusTemporaryRedirect)
 	}
 
-	provider := mx.providers[realmID]
-	http.Redirect(w, req, provider.cfg.AuthCodeURL(req.URL.Query().Get("state")), http.StatusTemporaryRedirect)
+	if provider, ok := mx.providers[realmID]; ok { // "default"
+		http.Redirect(w, req, provider.config.AuthCodeURL(req.URL.Query().Get("state")), http.StatusTemporaryRedirect)
+	} else {
+		// TODO: Decide best approach
+		discoveryURL := openid.BuildDiscoveryURL(realmID)
+		var buf bytes.Buffer
+		buf.WriteString(viper.GetString("services.auth-service.host"))
+		buf.WriteString(":")
+		buf.WriteString(viper.GetString("services.auth-service.port"))
+		buf.WriteString(viper.GetString("services.auth-service.discovery-url"))
+		buf.WriteString(realmID)
+
+		provider := openid.NewProvider(discoveryURL)
+		mx.providers[realmID] = provider
+		http.Redirect(w, req, provider.config.AuthCodeURL(req.URL.Query().Get("state")), http.StatusTemporaryRedirect)
+	}
 }
 
 func (mx *Mux) callback(w http.ResponseWriter, req *http.Request) {
-	realmID, _ := realm.FromContext(req.Context())
-	provider := mx.providers[realmID]
-	code := req.URL.Query().Get("code")
-
-	token, err := provider.cfg.Exchange(req.Context(), code)
-	if err != nil {
-		// handle err
+	realmID, ok := realm.FromContext(req.Context())
+	if !ok {
+		http.Redirect(w, req, "", http.StatusTemporaryRedirect)
 	}
 
-	// Extract the ID Token from OAuth2 token.
+	code := req.URL.Query().Get("code")
+	p := mx.providers[realmID]
+
+	token, err := p.config.Exchange(req.Context(), code)
+	if err != nil {
+		http.Redirect(w, req, "", http.StatusForbidden)
+	}
+
+	log.WithField("token", token).Debug("oauth2 token")
+
 	rawIDToken, ok := token.Extra("id_token").(string)
 	if !ok {
-		// handle missing token
+		http.Redirect(w, req, "", http.StatusForbidden)
 	}
 
-	// Parse and verify ID Token payload.
-	idToken, err := provider.verifier.Verify(req.Context(), rawIDToken)
+	idToken, err := p.verifier.Verify(req.Context(), rawIDToken)
 	if err != nil {
-		// handle error
+		http.Redirect(w, req, "", http.StatusForbidden)
 	}
 
-	// Extract custom claims
 	var claims struct {
 		Email    string `json:"email"`
 		Verified bool   `json:"email_verified"`
 	}
 	if err := idToken.Claims(&claims); err != nil {
-		// handle error
+		http.Redirect(w, req, "", http.StatusInternalServerError)
 	}
+
+	log.WithField("claims", claims).Debug("access token claims")
 }
 
 func (mx *Mux) expired(w http.ResponseWriter, req *http.Request) {
--- auth/mux.go
+++ auth/mux.go
@@ -1,33 +1,26 @@
 package auth
 
 import (
-	"context"
 	"net/http"
 
-	"github.com/coreos/go-oidc"
+	"github.com/damiannolan/auth-proxy/openid"
+	"github.com/damiannolan/auth-proxy/realm"
 	"github.com/go-chi/chi"
-	"github.com/spf13/viper"
-	"golang.org/x/oauth2"
+	"github.com/go-chi/chi/middleware"
 )
 
 // Mux is a request multiplexer
 type Mux struct {
 	path string
 
-	providers map[string]*Provider
-}
-
-// Provider bootstraps oauth2.Config and Provider
-type Provider struct {
-	cfg      oauth2.Config
-	provider *oidc.Provider
-	verifier *oidc.IDTokenVerifier
+	providers map[string]*openid.Provider
 }
 
 // NewMux returns a new http request multiplexer for the given path
 func NewMux(path string) *Mux {
 	mux := &Mux{
-		path: path,
+		path:      path,
+		providers: make(map[string]*openid.Provider),
 	}
 
 	return mux
@@ -38,42 +31,20 @@ func (mx *Mux) Path() string {
 	return mx.path
 }
 
-// RegisterProvider attempts to create a new OAuth2 Provider
-func (mx *Mux) RegisterProvider(tenantID string) {
-	authSvc := viper.GetString("services.auth-service.host") + viper.GetString("services.auth-service.port")
-	discoveryURL := authSvc + viper.GetString("services.auth-service.discovery-url") + tenantID
-	provider, err := oidc.NewProvider(context.Background(), discoveryURL)
-	if err != nil {
-		// handle err
-	}
-
-	oauth2Cfg := oauth2.Config{
-		ClientID:     viper.GetString("oauth.client-id"),
-		ClientSecret: viper.GetString("oauth.client-secret"),
-		RedirectURL:  viper.GetString("oauth.redirect-url"),
-		Endpoint:     provider.Endpoint(),
-		Scopes:       append(viper.GetStringSlice("oauth.scopes"), oidc.ScopeOfflineAccess, oidc.ScopeOpenID),
-	}
-
-	oidcCfg := &oidc.Config{
-		ClientID: viper.GetString("oauth.client-id"),
-	}
-
-	tokenVerifier := provider.Verifier(oidcCfg)
-
-	p := &Provider{
-		cfg:      oauth2Cfg,
-		provider: provider,
-		verifier: tokenVerifier,
-	}
-
-	mx.providers[tenantID] = p
+// RegisterProvider inserts the OpenID Connect Provider into the local cache
+func (mx *Mux) RegisterProvider(key string, p *openid.Provider) {
+	mx.providers[key] = p
 }
 
 // Handler bootstraps package routes and their respective HTTP HandlerFuncs
 // returning a standard http.Handler interface to be served independenly or mounted in a routing chain
 func (mx *Mux) Handler() http.Handler {
 	handler := chi.NewMux()
+
+	handler.Use(middleware.DefaultLogger)
+	handler.Use(middleware.Recoverer)
+	handler.Use(middleware.RequestID)
+	handler.Use(realm.DiscoveryMiddleware())
 
 	handler.Route(mx.Path(), func(r chi.Router) {
 		r.HandleFunc("/authorize", mx.authorize)
